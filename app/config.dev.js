d417fa545d175e88f1400743b742118b3a7e976c
Justin Sabourin <j.sabourin@mail.utoronto.ca>
2018-12-16 13:32:33 -0500

    init

--- app/config.dev.js
+++ app/config.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    repoPath: '/Users/justinsabourin/c09/teamunknown/repositories',
+    repoPath: '/Users/justinsabourin/projects/coder/repositories',
     mongoURL: 'mongodb://localhost/webeditor',
     memcachedURL: '127.0.0.1:11211',
     host: 'https://localhost:8082'
